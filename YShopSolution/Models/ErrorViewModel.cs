9b263dec0c8ad8d308bf1298a926c798e667eaf6
DoYen <75532626+DoYen2k@users.noreply.github.com>
2022-01-13 10:58:46 +0700

    Add project files.

---
+++ YShopSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YShopSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
