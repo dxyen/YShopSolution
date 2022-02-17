7210b3f63d570512fb343259984782e09391c9a0
Đỗ Xuân Yên <75532626+dxyen@users.noreply.github.com>
2022-02-17 14:38:53 +0700

    Add Interfaces

---
+++ YShopSolution.Utilities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace YShopSolution.Utilities
+{
+    public class Class1
+    {
+    }
+}
---
+++ YShopSolution.ViewModels/Request/Product/ProductUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YShopSolution.ViewModels.Request.Product
+{
+    public class ProductUpdateRequest
+    {
+    }
+}
