bdc79903bde6dddeda587eeb26c172b5c1903252
Đỗ Xuân Yên <75532626+dxyen@users.noreply.github.com>
2022-03-04 14:25:12 +0700

    Create Web API project

--- YShopSolution.Application/Interfaces/IProductService.cs
+++ YShopSolution.Application/Interfaces/IProductService.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YShopSolution.ViewModels;
+using YShopSolution.ViewModels.Common;
 using YShopSolution.ViewModels.Request.Product;
+using YShopSolution.ViewModels.ViewModels;
 
 namespace YShopSolution.Application.Interfaces
 {
@@ -13,7 +15,12 @@ namespace YShopSolution.Application.Interfaces
         Task<int> CreateAsync(ProductCreateRequest request);
         Task<int> UpdateAsync(ProductUpdateRequest request);
         Task<int> DeleteAsync(int id);
-        Task<int> GetAsync(int id);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<PagedResult<ProductViewModel>> GetAllPagingAsync(GetProductPagingRequest request);
+        Task<List<ProductViewModel>> GetByIdAsync(int productId);
+        Task<List<ProductViewModel>> GetBestSellerCakesAsync(int take);
+
+        Task<List<ProductViewModel>> GetAll();
 
     }
 }
--- YShopSolution.ViewModels/Request/Product/ProductUpdateRequest.cs
+++ YShopSolution.ViewModels/Request/Product/ProductUpdateRequest.cs
@@ -8,5 +8,10 @@ namespace YShopSolution.ViewModels.Request.Product
 {
     public class ProductUpdateRequest
     {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public decimal Price { set; get; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
 }
